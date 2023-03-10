fa52b45835320e5de8cb4ac69c95ee71bf0633b0
Hanzhang Cao <hz934@student.ubc.ca>
2023-03-10 09:53:11 -0800

    rewrite function to make it more general

--- rscript/average_numeric.R
+++ rscript/average_numeric.R
@@ -2,23 +2,20 @@
 #'
 #'Function to compare the mean values of the numerical attributes
 #'
-#'@param data the training set with a list of numerical attributes
+#'@param dataset the training set with a list of numerical attributes
+#'@param class_col the name for the column to group by
 #'
 #'@export
 #'
 #'@example
-#'avg_numeric<-function(training_set)
+#'avg_numeric(iris,Species)
 #'
-avg_numeric<- function(data){
-  # returns a summary table of Average values of the numerical attributes 
-  summary_averages <- data %>%
-    group_by(diagnosis) %>%
-    summarize(count=n(), 
-              mean_age=mean(age), 
-              mean_resting_bp=mean(resting_bp), 
-              mean_cholesterol=mean(cholesterol),
-              mean_max_heart_rate=mean(max_heart_rate), 
-              mean_old_peak=mean(old_peak),
-              mean_no_vessels_colored=mean(no_vessels_colored, na.rm = TRUE))
+library(dplyr)
+avg_numeric<- function(dataset, class_col){
+  # returns a summary table of Average values of the numerical variables 
+  summary_averages <- dataset |>
+    group_by({{class_col}}) |>
+    summarise_if(is.numeric, mean, na.rm = TRUE)
   return(summary_averages)
 }
+
