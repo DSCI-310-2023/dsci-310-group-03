1b4cc71e1a5413b19c74e27f42757294b27a4207
Kaylie Nguyen <anhndq01@student.ubc.ca>
2023-03-10 11:22:47 -0800

    Update function abstraction for the model

--- R/05-build_model.R
+++ R/05-build_model.R
@@ -1,6 +1,6 @@
 #'Building model
 #'
-#'Function to build knn model with cross validation
+#'Function build_model() to build knn model with cross validation
 #'
 #'@param recipe a recipe to prepare the data for modelling
 #'@param optimal a variable that takes in the string "None" or "Yes" to specify whether
@@ -10,26 +10,48 @@
 #'
 #'@return a fitted knn model
 #'
+#''@example build_model(image_recipe, NULL)
+#'
+#''Function get_best_fit() to obtain the statistics of the best knn_fit after tuning
+#'@param model a fitted model, such as the return from the previous function
+#'
+#'@return a dataframe of the statistics for the optimal knn model
+#'
+#'@example get_best_k(knn_fit)
+#'
 #'@export
 #'
-#'@example build_model(image_recipe, NULL)
+#
 
 build_model <- function(recipe, optimal, k) {
   if(optimal=="None"){
     knn_spec <- nearest_neighbor(weight_func = "rectangular", neighbors = tune()) %>%
       set_engine("kknn") %>%
       set_mode("classification")
+    knn_fit <- workflow() %>%
+      add_recipe(recipe) %>%
+      add_model(knn_spec) %>%
+      tune_grid(resamples = {{vfold}}, grid={{gridvals}})%>%
+      collect_metrics()
     }
-  else {
-    knn_spec <- nearest_neighbor(weight_func = "rectangular", neighbors = 13) %>%
+  else if (optimal == 'Yes') {
+    knn_spec <- nearest_neighbor(weight_func = "rectangular", neighbors = k) %>%
       set_engine("kknn") %>%
       set_mode("classification")
+    knn_fit <- workflow() %>%
+      add_recipe(recipe) %>%
+      add_model(knn_spec) %>%
+      fit(data = {{training_set}})
    }
-  knn_fit <- workflow() %>%
-    add_recipe(recipe) %>%
-    add_model(knn_spec) %>%
-    tune_grid(resamples = {{vfold}}, grid={{gridvals}})%>%
-    collect_metrics()
+ 
   
   return(knn_fit)
+}
+
+get_best_fit <- function(model) {
+  best_fit <- knn_fit %>%
+    filter(.metric == "accuracy") %>%
+    arrange(desc(mean)) %>% 
+    slice(1)
+  return(best_fit)
 }
