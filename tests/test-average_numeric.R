2021377b198ced16fe080ec5c2f542a0ff1fd2e8
Karlie Truong <karlietruong@gmail.com>
2023-03-21 15:19:22 -0700

    remove tests folder

--- tests/test-average_numeric.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-source("../R/average_numeric.R")
-
-test_that("Return the summary table of mean grouped by income status",
-          {expect_equal(expected, avg_numeric(incomedata,status))})
-
