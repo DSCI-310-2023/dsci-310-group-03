b6d70c0ae0c4835aa3e4bd1a4c1331eb84008b78
Karlie Truong <karlietruong@gmail.com>
2023-03-22 17:45:27 -0700

    increase abstraction_histogram robustness

--- R/abstraction_histogram.R
+++ R/abstraction_histogram.R
@@ -2,29 +2,55 @@
 #'
 #' Create a general function to remove repetitive ggplot functions
 #'
+#' @param data the data frame
+#' @param class_column the groups that we are going to grouped the variables by
+#' @param binwidth a number that indicates 
+#'                 the bin width of the individual histograms
+#' @param ncol the number of columns when arranging the plots
 #'
-#'#'
-#' @param x_aes x-value, factorial variable of the histogram
-#' @param x_lab label for x-value of the histogram
-#' @param data data set to study for the histogram
-#' @param fill_aes variable to study for the histogram, "Diagnosis"
-#' @param y_lab label for y-value of the histogram
-#' @param fill_lab label for the variable, "Diagnosis" to study for the histogram
-#'
-#' @return A histogram based on study on diagnosis given different x-variable
-#'
-#' @export
+#' @return A histogram of all numeric variables that is 
+#'         grouped by the class_Column
 #'
 #' @examples
-#' abs_hist(training_set$age, "Age", training_set, training_set$diagnosis, "Percentage of Observations", "Diagnosis")
-abs_hist <- function(x_aes,  x_lab, data, fill_aes, y_lab, fill_lab) {
-  # returns a histogram 
-  histogram <- ggplot(data, aes(x = x_aes, fill = fill_aes)) + 
-    geom_histogram(position="dodge") +
-    labs(x = x_lab, y = y_lab, fill = fill_lab)+
-    theme(text = element_text(size = 10)) +
-    scale_fill_brewer(palette = "Paired")
-  return(histogram)
+#' plot_hist(iris, Species, binwidth = 0.25)
+
+plot_hist <- function(data, 
+                     class_column, 
+                     binwidth = 20,
+                     ncol = 2) {
+  
+  # Create a new empty list
+  plots <- list()
+  
+  # Generate a plot for each variable that is not numeric
+  for (i in 1:ncol(data)) {
+    
+    if (!is.numeric(data[ ,i])) {
+      next
+    }
+    
+    x_axis <- data[ ,i]
+    x_label <- colnames(data)[i]
+    fill <- dplyr::pull(data, {{ class_column }})
+    
+    histogram <- data |>
+      ggplot2::ggplot(ggplot2::aes(x = x_axis,
+                                   fill = fill)) +
+      ggplot2::geom_histogram(binwidth = binwidth,
+                              position = "dodge") +
+      ggplot2::labs(x = print(x_label), 
+                    y = "Frequency", 
+                    fill = names(fill)) +
+      ggplot2::theme(text = ggplot2::element_text(size = 10)) +
+      ggplot2::scale_fill_brewer(palette = "Paired")
+    
+    plots[[i]] <- histogram
+  
+  }
+  
+  # return the arranged plots with labels
+  return(cowplot::plot_grid(plotlist = plots,
+                            ncol = ncol,
+                            labels = "auto",
+                            label_size = 10))
 }
-
-
