6ceffd1f2815f78a978cf03713201ab7de08c8d4
Ser Jie <serjieserjie@gmail.com>
2023-03-24 10:18:24 -0700

    Added transformation part for training and testing set in script 2

--- src/preprocess_data.R
+++ src/preprocess_data.R
@@ -1,15 +1,16 @@
 'Performs data cleaning, pre-processing and transforming for data (from https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data).
-Writes the training and test data to separate feather files.
+Writes the training and test data to separate files.
 
-Usage: src/preprocess_data.r --input=<input> --out_dir=<out_dir>
+Usage: src/preprocess_data.r --input=<input> --out_train=<out_train> --out_test=<out_test>
   
 Options:
---input=<input>       Path (including filename) to raw data (csv file)
---out_dir=<out_dir>   Path to directory where the processed data should be written
+--input=<input>           Path (including filename) to raw data (csv file)
+--out_train=<out_train>   Path to directory where the processed training data should be written
+--out_test=<out_test>     Path to directory where the processed testing data should be written
 
 Example:
-results/training_set.csv: src/preprocess_data.r 
-	Rscript src/preprocess_data.r --input="../data/raw/heart_disease_data.csv" --out_dir="../results/training_set.csv"
+results/training_set.csv results/testing_set.csv: src/preprocess_data.r 
+	Rscript src/preprocess_data.r --input="../data/raw/heart_disease_data.csv" --out_train="../results/training_set.csv" --out_test="../results/testing_set.csv"
 
 ' -> doc
 
@@ -75,8 +76,40 @@ main <- function(input, out_dir){
   
   training_set
   
+  # Converting categorical variable to numeric data
+  transform_numeric <- function(df) {
+    mutated <- mutate(
+      df,
+      
+      exercise_pain = as.character(exercise_pain),
+      exercise_pain = replace(exercise_pain, exercise_pain == "fal", "0"),
+      exercise_pain = as.numeric(replace(exercise_pain, exercise_pain == "true", "1")),
+      
+      slope = as.character(slope),
+      slope = replace(slope, slope == "down", "-1"),
+      slope = replace(slope, slope == "flat", "0"),
+      slope = as.numeric(replace(slope, slope == "up", "1")),
+      
+      # Below values are "non-standard" values for a better scale
+      thal = as.character(thal),
+      thal = replace(thal, thal == "rev", "4"),
+      thal = replace(thal, thal == "norm", "0"),
+      thal = as.numeric(replace(thal, thal == "fix", "5")),
+      
+      resting_ecg = as.character(resting_ecg),
+      resting_ecg = replace(resting_ecg, resting_ecg == "abn", "4"),
+      resting_ecg = replace(resting_ecg, resting_ecg == "norm", "0"),
+      resting_ecg = as.numeric(replace(resting_ecg, resting_ecg == "hyp", "5")),
+    )
+    return(mutated)
+  }
+  
+  training_set <- transform_numeric(training_set)
+  testing_set <- transform_numeric(testing_set)
+  
   # write training data to csv files
+  write_csv(training_set, out_dir)
   write_csv(training_set, out_dir)
 }
 
-main(opt[["--input"]], opt[["--out_dir"]])
+main(opt[["--input"]], opt[["--out_train"]], opt[["--out_test"]])
