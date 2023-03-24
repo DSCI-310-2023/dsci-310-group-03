8482b74716115ca20e3537e2819f9acc47ea1a10
Karlie Truong <karlietruong@gmail.com>
2023-03-23 17:23:24 -0700

    add helper-build_model; rename tests; change path

--- tests/testthat/test-load_data.R
+++ tests/testthat/test-load_data.R
@@ -7,16 +7,16 @@ data_from_function <- load_data(
   na_values = "?"
 )
 
-# tests
-testthat::test_that("columns have expected column names and same number of columns", {
+# Tests -----------------------------------------------------------------------
+testthat::test_that("load_data returns a dataframe with same column names", {
   testthat::expect_equal(column_names, colnames(data_from_function))
 })
 
-testthat::test_that("returned data frame has the same number of rows", {
+testthat::test_that("load_data returns the same dataframe with read_delim", {
   testthat::expect_equal(data_from_function, data_from_read_delim)
 })
 
-testthat::test_that("function return a data frame", {
+testthat::test_that("load_data returns a dataframe", {
   testthat::expect_true(is.data.frame(data_from_function))
 })
 
