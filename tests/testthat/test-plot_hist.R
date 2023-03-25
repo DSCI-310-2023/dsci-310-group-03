57e63c27981f34cd236903525515886117409123
Karlie Truong <karlietruong@gmail.com>
2023-03-25 12:50:00 -0700

    add title function

--- tests/testthat/test-plot_hist.R
+++ tests/testthat/test-plot_hist.R
@@ -1,8 +1,13 @@
 source("../../R/plot_hist.R")
 
-test_that("ggplot2 histogram works", {
+testthat::test_that("ggplot2 histogram works", {
   vdiffr::expect_doppelganger("histograms from ggplot",
                               histograms_ggplot)
   vdiffr::expect_doppelganger("histograms from function",
                               plot_hist(iris, Species))
+})
+
+testthat::test_that("`plot_hist` should return an error when `title` is not
+                    a string", {
+  testthat::expect_error(plot_hist(iris, Species, title))
 })
